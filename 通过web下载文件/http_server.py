59d9049fccbffda05d9cdcef3ef954ef993c417d
yjz <yangjunzhi@mail.cdu.edu.cn>
2018-06-04 11:38:56 +0800

    通过web下载文件

--- 通过web下载文件/http_server.py
+++ 通过web下载文件/http_server.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 #在运行的目录下生成http服务，访问并下载文件。
 import os
+import re
 
 from http.server import \
 	BaseHTTPRequestHandler, HTTPServer
@@ -20,6 +21,7 @@ class MyHandler(BaseHTTPRequestHandler):
 					self.wfile.write(('<li><a href='+self.path+quote(i)+'>'+i+'</a></li>').encode())
 				else:
 					self.wfile.write(('<li><a href='+self.path+quote(i)+'/>'+i+'</a></li>').encode())
+			self.wfile.write(('<li><form id="form1" action="upload" target="frame1" method="post" enctype="multipart/form-data"> <input type="file" name="file"> <input type="submit" value="上传"> </form></li>').encode())
 		except:
 			#self.send_error(404,'File not Found:%s'%self.path)
 			f = open(os.getcwd()+unquote(self.path).replace('/','\\'),'rb')
@@ -28,6 +30,33 @@ class MyHandler(BaseHTTPRequestHandler):
 			self.end_headers()
 			self.wfile.write(f.read())
 			f.close()
+			
+	def do_POST(self):
+		#try:
+			r = 'filename="(.*)"'
+			self.send_response(200)
+			self.send_header('Content-type','text/html; charset=utf-8')
+			self.end_headers()
+			self.wfile.write(('上传成功').encode())
+			a = self.rfile.readline()
+			b = self.rfile.readline()
+			self.rfile.readline()
+			self.rfile.readline()
+			filename = re.search(r,b.decode('utf-8')).group(1)
+			print(filename)
+			with open (filename,'ab') as f:
+				while True:
+					read=self.rfile.readline()
+					if '------WebKitFormBoundary' in str(read):
+						break
+					print(read)
+					if read:
+						f.write(read)
+				f.close()
+
+		# except:
+			# print(1)
+			
 
 def main():
 	try:
